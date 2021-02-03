8db00eb7a272d6e3afbec1455b71dc5360ca157a
eveningwater <854806732@qq.com>
2021-02-03 14:19:25 +0800

    更新了代码

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,17 @@
   <Background v-model:snowStatus="show" />
   <div class="theme">
     <div class="ew-content-box">
-      <ewFolderMenu @on-menu-click="onClick" v-click-outside="clickoutside" />
+      <div class="container">
+        <ewFolderMenu @on-menu-click="onClick" v-click-outside:[outsideContainer]="clickoutside" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { onMounted, reactive, toRefs } from "vue";
+import { onMounted, reactive, ref, toRefs } from "vue";
 import Background from "./views/Background/Background.vue";
+import { nextTick } from 'process';
 export default {
   components: {
     Background
@@ -18,15 +21,22 @@ export default {
     const state = reactive({
       show: false
     });
+    const outsideContainer= ref('');
     const onClick =(status) => {
       console.log(status)
     }
     const clickoutside = () => {
       console.log('点击该元素之外的区域触发')
     }
+    onMounted(() => {
+      nextTick(() => {
+        outsideContainer.value = document.querySelector('.container');
+      })
+    })
     return {
       ...toRefs(state),
       onClick,
+      outsideContainer,
       clickoutside
     };
   },
@@ -34,5 +44,9 @@ export default {
 </script>
 
 <style lang="less">
+.ew-content-box > .container{
+  width: 500px;
+  height: 500px;
+}
  @import "./styles/app.less";
 </style>
